d162391ca23a940a6581b5ef4af62d0b725d8266
Kerautret <kerautre@loria.fr>
2015-05-12 11:15:36 +0200

    test run

--- app.py
+++ app.py
@@ -189,8 +189,7 @@ class app(base_app):
         this one needs no parameter
         """
         f = open(self.work_dir+"output.txt", "w")
-        command_args = ['test.sh','-i', 'input_0.vol', '-c', 'input_0.sdp', '-m', rmin,'-M', rmax,
-                        '--alphaImageHeight', a, '-s', 1, '-o', 'resp.pgm', '--skipFirstSlice', 30  ]
+        command_args = ['test.sh','-i', 'input_0.vol', '-c', 'input_0.sdp', '-m', rmin,'-M', rmax, '--alphaImageHeight', a, '-s', 1, '-o', 'resp.pgm', '--skipFirstSlice', 30  ]
 
         self.runCommand(command_args, f, comp = " > output.txt" )
         f.close()
