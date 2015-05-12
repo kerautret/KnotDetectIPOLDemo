50aa1a58811fb5fa89af6612c89853f22ea4ba18
Kerautret <kerautre@loria.fr>
2015-05-12 11:31:36 +0200

    test run

--- app.py
+++ app.py
@@ -189,12 +189,9 @@ class app(base_app):
         this one needs no parameter
         """
         f = open(self.work_dir+"output.txt", "w")
-        command_args = ['apply.sh','-i', 'input_0.vol', '-c', 'input_0.sdp', '-m', str(rmin),'-M', str(rmax), '--alphaImageHeight', str(a), '-s', "1", '-o', 'resp.pgm', '--skipFirstSlice', "30"  ]
+        command_args = ['singleKnotsDetection','-i', 'input_0.vol', '-c', 'input_0.sdp', '-m', str(rmin),'-M', str(rmax), '--alphaImageHeight', str(a), '-s', "1", '-o', 'resp.pgm', '--skipFirstSlice', "30"  ]
 
-        self.runCommand(command_args, f,  comp = " > output.txt" )
-        f.close()
-        f = open(self.work_dir+"commands.txt", "w")
-        f.write(self.list_commands)
+        self.runCommand(command_args, f )
         f.close()
         return
 
