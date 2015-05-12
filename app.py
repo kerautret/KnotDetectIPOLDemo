3933ee627c47149d95bfeeec54b6768ebca3a482
Kerautret <kerautre@loria.fr>
2015-05-12 12:04:54 +0200

    test run

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ class app(base_app):
         this one needs no parameter
         """
         f = open(self.work_dir+"output.txt", "w")
-        command_args = ['apply.sh','-i', 'inputVol_0.vol', '-c', 'inputVol_0.sdp', '-m', str(rmin),'-M', str(rmax), '--alphaImageHeight', str(a), '-s', "1", '-o', 'resp.pgm', '--skipFirstSlice', "30"  ]
+        command_args = ['apply.sh','-i', self.work_dir +'inputVol_0.vol', '-c', self.work_dir +'inputVol_0.sdp', '-m', str(rmin),'-M', str(rmax), '--alphaImageHeight', str(a), '-s', "1", '-o', self.work_dir +'resp.pgm', '--skipFirstSlice', "30"  ]
 
         p = self.run_proc(command_args, env={'LD_LIBRARY_PATH' : self.bin_dir})
         self.wait_proc(p, timeout=self.timeout)
